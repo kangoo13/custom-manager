6c3396907ad88afd683b85a3bafeaf5e30119a89
Aurélien <aurelien.schiltz@epitech.eu>
2016-10-17 11:37:05 +0200

    First push

---
+++ README.md
@@ -0,0 +1,1 @@
+Loads one random proxy from list.txt and use it for every request of the scrapy script
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'custom-manager',
+  packages = ['custom-manager'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A scrapy proxies handling',
+  author = 'Aurelien Schiltz',
+  author_email = 'aurelien.schiltz@epitech.eu',
+  url = 'https://github.com/kangoo13/custom-manager', # use the URL to the github repo
+  download_url = 'https://github.com/kangoo13/custom-manager/tarball/0.1', # I'll explain this in a second
+  keywords = ['scrapy', 'proxies', 'proxy'], # arbitrary keywords
+  classifiers = [],
+)
